71379c6641bbde39d0b7d0722d42ff5fda990c19
killbulle <marco.godin@gmail.com>
2020-03-25 17:11:36 +0100

    Updates

---
+++ precache-manifest.67ee7586702f9aa6755e5e52223ab45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf85d3a8f5b538e4c7b572c69e59867",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08688d668d7c1012c56f",
+    "url": "/monsters-rolodex/static/css/main.c941b404.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08688d668d7c1012c56f",
+    "url": "/monsters-rolodex/static/js/main.e09c4083.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
